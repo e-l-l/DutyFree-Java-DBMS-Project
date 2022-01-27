724559dfccd06497bca963e1cb1dd82e2ddaecd8
INSAEN <pratham1986@gmail.com>
2022-01-28 02:58:14 +0530

    finished app with jar file

--- src/dutyfree/Dutyfree.java
+++ src/dutyfree/Dutyfree.java
@@ -14,7 +14,7 @@ public class Dutyfree {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new logon().setVisible(true);
     }
     
 }
